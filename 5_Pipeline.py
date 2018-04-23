eaee12a16805941fcebba3a1e5221d0902eabed8
HYUN <changja00@gmail.com>
2018-04-23 18:23:43 +0900

    7

--- 5_Pipeline.py
+++ 5_Pipeline.py
@@ -15,8 +15,8 @@ def read_file(path_to_file):
 
 
 def filter_chars_and_normalize(str_data):
-    pattern = re.compile('[\w_]+')
-    return pattern.sub(' ', str_data).lower()
+    pattern = re.compile('[\w_]+')  # 정규 표현식
+    return pattern.sub(' ', str_data).lower()  # 소문자로 바꿔
 
 
 def scan(str_data):
@@ -50,4 +50,102 @@ def print_all(word_freqs):
         print_all(word_freqs[1:])
 
 
-print_all(sort(frequencies(remove_stop_words(scan(filter_chars_and_normalize(read_file(sys.argv[1]))))))[0:25])
+print_all(
+    sort(
+        frequencies(
+            remove_stop_words(
+                scan(
+                    filter_chars_and_normalize(
+                        read_file(sys.argv[1])
+                    )
+                )
+            )
+        )
+    )[0:25])
+
+# 해설
+# - 파이프 라인 형식은 공장 파이프라인 모델을 본뜬 것으로 각 위치 또는 상자에서 그것을 지나는 데이터를 맡아 특정 작업을 수행한다
+# - 가장 순수한 형태 면에서 파이프라인 형식은 수학적 함수 이론을 충실히 반영한 것이다
+# - 입력으로 함수를 취하거나 함수를 출력하는 함수를 고차 함수(High-order function) 라고 한다
+# - 파이프 라인 프로그래밍 형식에서는 모든 것을 입력 집합 하나를 출력 집합 하나에 매핍하는 연관 관계로 바라봄으로써
+#   이러한 수학적 순수성을 당성하려 한다
+# - 파이프라인에서는 시작할 때 계산에 대한 입력원과 마지막에 출력을 받는 곳 외에 상자화된 함수 외부의 세계는 존재하지 않는다
+#   프로그램은 상자화된 함수와 함수 합성으로 표현한다
+# - 멱등성이 보장된다
+# - 예제 프로그램에서는 모든 함수가 인자가 단 하나만 있지만 일반적으로 함수에는 여러 인자가 있을 수 있다
+#   그러나 인자가 여러 개인 함수는 모두 커링(Currying)이라는 기법을 통해 일련의 단일 값 고차 함수로 변형 할 수 있다
+def f1(x, y, z):  # 커링 미적용
+    return x * y + z
+
+
+f1(2, 3, 4)
+
+
+def f(x):  # 커링 적용
+    def g(y):
+        def h(z):
+            return x * y + z
+
+        return h
+
+    return g
+
+
+f(2)(3)(4)
+
+# 시스템 설계 관점에서 본 이러한 형식
+# - 파이프 라인 형식은 특히 해당 문제의 특성이 파이프라인으로 모델링할 수 있는 문제에 적합하다
+#   그래프 검색과 같은 인공지능 알고리즘인 A* 등은 이 범주에 속한다
+#   컴파일러 및 다른 언어 처리기에도 적합한데 그래프와 트리 구조를 바탕으로 한 기능으로 구성하는 경향이 있기 때문이다
+# - 문제에 대한 적합성 외에도 이 형식을 사용하는 것이 좋은 이유는 단위 테스트와 병행성이다
+#   이 형식을 활용한 프로그램은 단위 테스트하기가 매우 쉽다
+#   테스트 할 수 있는 상자화된 함수 외부에 어떤 상태도 보관하지 않으므로 해당 테스트를 한번 또는 여러 번 다른 순서로 실행 하더라도
+#   항상 같은 결과가 만들어지기 때문이다
+#   문제를 파이프라인 형식으로 적절히 표현할 수 있다면 그렇게 하는 것이 좋다
+
+# 역사적
+# -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 6_CodeGolf.py
@@ -0,0 +1,26 @@
+# 코드골프 (Code Golf)
+
+# 제약조건
+# 코드줄 수를 가능한 한 적게 한다
+
+import re, sys, collections
+
+stops = open('/Users/hyun/Desktop/BackgroundInfo/Book/Programing_Pattern/stop_words.txt').read().split(',')
+words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
+counts = collections.Counter(w for w in words if w not in stops)
+for (w, c) in counts.most_common(25):
+    print(w, '-', c)
+
+
+# 해설
+# - 이 형식의 주된 관심사는 간결함이다
+# - 간결함이 유일한 목표라면 이 형식으로 작성된 코드가 결과적으로 이해해기 어려운 명령이 나열된 매우 긴 몇줄의 코드가 되는 것이 이상하지 않다
+# - 하지만 간결함을 적절히 활용하면 작은 크기로 인해 꽤 우아하고 읽기 쉬운 프로그램을 만들어 낼 수도 있다
+# - 코드 줄 수 측면에서 간결함은 이미 다른 누군가각 만들어 둔 강력한 추상화를 사용하는 것과 관련돼있다
+
+
+# 시스템 설계 관점에서 본 이러한 형식
+# - 소프트웨어 업계에서 사용하는 가장 인기 있는 측정 지표 중 하나는 소스코드 줄 수(Source Lines fo Code:SLOC)이다
+#   좋든 나쁘든 SLOC는 비용 산정 개발자 생산성 유지보수성 및 다른 여러 관리적인 측면의 관심사에 대한 간접적인 지표로 널리 사용된다
+# - 가능한 한 가장 짧은 프로그램을 다양한 프로그래밍 언어로 만드는 기술에는 심지어 '코드 골프라는 이름도 있다
+# - 하지만 프
